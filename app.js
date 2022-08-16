1649ea07ccc93229ce55d3525b55252ff524d32f
Malay <malayacharya21@gmail.com>
2022-08-17 01:41:32 +0530

    Understood how global objects differ from globally avaialable objects

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const Circle = require("./circle");
+console.log(process.env.PWD);
 
-const circle = new Circle();
-console.log(circle.area(5));
-console.log(circle.circumference(5))
+
+
+// const Circle = require("./circle");
+
+// const circle = new Circle();
+// console.log(circle.area(5));
+// console.log(circle.circumference(5))
 
 
 // const http = require("http");
