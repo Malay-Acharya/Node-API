5f1db9c14dfa0e72760268204edab1890a94635a
Malay <malayacharya21@gmail.com>
2022-08-17 01:36:17 +0530

    common js requiring files

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const http = require("http");
+const Circle = require("./circle");
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(
-        JSON.stringify({
-            data:"Hello World"
-        })
-    );
-});
+const circle = new Circle();
+console.log(circle.area(5));
+console.log(circle.circumference(5))
 
-server.listen(3000);
+
+// const http = require("http");
+
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200, {"Content-Type": "application/json"});
+//     res.end(
+//         JSON.stringify({
+//             data:"Hello World"
+//         })
+//     );
+// });
+
+// server.listen(3000);
+
 
 
 
