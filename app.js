c8e7deb141aaa4573e6b58a7d697881dae43415e
Malay <malayacharya21@gmail.com>
2022-08-17 01:27:16 +0530

    Created a basic http server

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(
+        JSON.stringify({
+            data:"Hello World"
+        })
+    );
+});
+
+server.listen(3000);
+
+
+
+
