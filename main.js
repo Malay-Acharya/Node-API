317631592c5499497a15bfef1ec4755796d39f19
Malay <malayacharya21@gmail.com>
2022-08-17 03:00:52 +0530

    Learned about event emitter

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const EventEmitter = require("events");
+
+const myEvent = new EventEmitter();
+
+myEvent.on("test-event", () =>{
+    console.log('this event is listening');
+})
+
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req,res) =>{
+    myEvent.emit("test-event")
     res.send("Hello World")
 })
 
