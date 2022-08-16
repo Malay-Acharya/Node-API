0d066b773a4db13156ecb20abf75281e780de0b0
Malay <malayacharya21@gmail.com>
2022-08-17 02:24:46 +0530

    Worked with html and http together

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const http = require("http");
+const fs= require("fs")
+
 const PORT = 3000;
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200, {"Content-Type":"application/json"});
-    res.end(
-        JSON.stringify({
-            data:"Hello World",
+    if(req.url === "/"){
+        res.writeHead(200, {"Content-Type":"text/html"});
+        fs.readFile("pages/home.html", "utf8", (err, data) =>{
+            if (err) throw err;
+            res.write(data)
+            res.end()
         })
-    )
-    res.end();
+    }
+    else if(req.url === "/about"){
+        res.writeHead(200, {"Content-Type":"text/html"});
+        fs.readFile("pages/about.html", "utf8", (err, data) =>{
+            if (err) throw err;
+            res.write(data)
+            res.end()
+        })
+    }else{
+        res.writeHead(404, {"Content-Type":"text/html"});
+        fs.readFile("pages/404.html", "utf8", (err, data) =>{
+            if (err) throw err;
+            res.write(data)
+            res.end()
+        })
+    }
+    
 });
 
 console.log(`server running on PORT ${PORT}`);
