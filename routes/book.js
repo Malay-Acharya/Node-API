c7351eefbcdddb8b7293fde5e33246017cafca54
Malay <malayacharya21@gmail.com>
2022-08-20 18:10:56 +0530

    crud operations and status codes

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bookrouter = express.Router();
 const connect = require("../database/db")
+var ObjectId = require("mongodb").ObjectId
 
 bookrouter.route('/book')
 .get(async (req,res) =>{
@@ -11,16 +12,25 @@ bookrouter.route('/book')
 post(async (req,res) =>{  
     const db = await connect();
     await db.collection('book').insertOne(req.body);
-    res.json({data: "Book is stored"})
+    res.status(201).json({data: "Book is stored"})
 })
 
 bookrouter.route("/book/:id")
-.get((req,res)=>{
-    res.send(`single book of id : ${req.params.id}`)
-}).patch((req,res)=>{
-    res.send(`single book of id : ${req.params.id}`)
-}).delete((req,res)=>{
-    res.send(`single book of id : ${req.params.id}`)
+.get(async (req,res)=>{
+    const id = req.params.id;
+    const db = await connect();
+    const book = await db.collection('book').find({_id : ObjectId(id)}).toArray();
+    res.json(book);
+}).patch(async (req,res)=>{
+    const _id = ObjectId(req.params.id);
+    const db = await connect();
+    await db.collection('book').updateOne({_id}, {$set: req.body});
+    res.json({data: "Book is updated"});
+}).delete(async (req,res)=>{
+    const _id = ObjectId(req.params.id);
+    const db = await connect();
+    await db.collection('book').deleteOne({_id})
+    res.status(204).json({data: "Book is deleted"});
 })
 
 
